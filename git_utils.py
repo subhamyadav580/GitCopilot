ac8c6a9880d753c9b3819e69a3f6c01819aedf25
subhamyadav580 <subhamyadav580@gmail.com>
2026-01-23 01:56:52 +0530

    Update print statement to include os.walk path

--- git_utils.py
+++ git_utils.py
@@ -30,7 +30,7 @@ class GitCopilotUtils:
 
     def find_git_repos(self, state: GithubCopilotAgent) -> GithubCopilotAgent:
         repos = []
-        print("Searching for git repositories...")
+        print("Searching for git repositories...", os.walk("~"))
         for dirpath, dirnames, _ in os.walk("~"):
             print("dirpath, dirnames:", dirpath, dirnames)
             dirnames[:] = [d for d in dirnames if d not in self.EXCLUDE_DIRS]
